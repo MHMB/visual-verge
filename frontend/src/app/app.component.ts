7dfced132f0a2271a6864672977ded4ec246f683
mhmb <m.h.motie@gmail.com>
2024-11-18 02:28:39 +0330

    feat: add nav bar component

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import {Component} from '@angular/core';
+import {NavBarComponent} from './nav-bar/nav-bar.component'
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [NavBarComponent],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <nav-bar></nav-bar>
+      </section>
+    </main>
+  `,
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'homes';
+}
